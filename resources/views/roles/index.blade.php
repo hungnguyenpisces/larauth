92ac06ef32c336c6b0dc39c903809a4b9d9bd9fd
Van Hung Nguyen <hungnguyenpisces@gmail.com>
2022-08-07 23:20:46 +0700

    v1

---
+++ resources/views/roles/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+@section('content')
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header">
+            <h2 class="card-title">Roles Management</h2>
+            <div class="card-tools">
+                <a class="btn btn-success" href="{{ route('roles.create') }}"><i class="fas fa-plus-square"></i> New Role</a>
+            </div>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body table-responsive">
+            <table class="table table-striped table-bordered">
+                <tr class="bg-blue text-center">
+                    <th width="50px">No</th>
+                    <th>Name</th>
+                    <th width="200px">Action</th>
+                </tr>
+                @foreach ($roles as $key => $role)
+                <tr>
+                    <td class="text-center">{{ ++$key }}</td>
+                    <td class="text-center">{{ $role->name }}</td>
+                    <td class="text-center">
+
+                        <form action="{{ route('roles.destroy',$role->id) }}" method="POST">
+                            <a class="btn btn-sm btn-info" href="{{ route('roles.show',$role->id) }}">Show</a>
+                            @can('role-edit')
+                            <a class="btn btn-sm btn-primary" href="{{ route('roles.edit',$role->id) }}">Edit</a>
+                            @endcan
+                            @can('role-delete')
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                            @endcan
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </table>
+
+        </div>
+    </div>
+    <!-- /.card -->
+</div>
+@endsection
